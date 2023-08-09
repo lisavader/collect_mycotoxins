7a3e74a2f74de8c42bb996b48bf25c05dcc846fe
Lisa Vader <lisavader@users.noreply.github.com>
2023-08-09 13:27:15 +0200

    Style changes

--- find_mycotoxin_bgcs_mibig.py
+++ find_mycotoxin_bgcs_mibig.py
@@ -1,6 +1,6 @@
 import argparse
 import logging
-import pandas as pd  
+import pandas as pd
 import json
 import glob
 import os
@@ -16,7 +16,7 @@ class DatabaseID:
         self.database: str = database
         self.id: str = id
         self.url: str = ""
-     
+
     def lookup_url(self) -> requests.Response:
         """ Send HTTP request and handle HTTP errors"""
         for retries in range(3):
@@ -36,7 +36,7 @@ class DatabaseID:
                     continue
             break
         return(response)
-    
+
     def get_inchikey(self) -> str:
         """ Search database to retrieve inchikey """
         inchikey = None
@@ -46,13 +46,13 @@ class DatabaseID:
                 self.url="https://www.ebi.ac.uk/chembl/api/data/molecule?molecule_chembl_id__exact="+self.id+"&format=json"
                 response=self.lookup_url()
                 inchikey=response.json()["molecules"][0]["molecule_structures"]["standard_inchi_key"]
-            
+
             elif self.database == "chebi":
                 self.url="https://www.ebi.ac.uk/webservices/chebi/2.0/test/getCompleteEntity?chebiId="+self.id
                 response=self.lookup_url()
                 soup = BeautifulSoup(response.text, 'xml')
                 inchikey=soup.find("inchiKey").text
-            
+
             elif self.database == "npatlas":
                 self.url="https://www.npatlas.org/api/v1/compound/"+self.id
                 response=self.lookup_url()
@@ -67,7 +67,7 @@ class DatabaseID:
                 session=chemspipy.ChemSpider(api_key)
                 compound=session.get_compound(self.id)
                 inchikey=compound.inchikey
-   
+
             elif self.database == "pubchem":
                 self.url="https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/"+self.id+"/property/InChIKey/TXT"
                 response=self.lookup_url()
@@ -77,7 +77,7 @@ class DatabaseID:
             logging.warning(self.database+":"+self.id+": "+repr(e))
 
         return inchikey
-      
+
 class Compound:
     """ A compound within the MIBiG database """
     def __init__(self,mibig_accession,index,name):
@@ -87,13 +87,13 @@ class Compound:
         self.database_ids: List[DatabaseID] = []
         self.inchikey: str = None
         self.mycotoxin: bool = False
-    
+
     def is_mycotoxin(self,mycotoxin_inchikeys):
         """ Use associated database ids to find the inchikey and check if the compound is a mycotoxin """
         #Reorder database ids (to search in order)
         database_order = {"chembl":0,"chebi":1,"npatlas":2,"chemspider":3,"pubchem":4}
         self.database_ids.sort(key=lambda val: database_order[val.database])
-        
+
         #Search databases for inchikey
         for database_id in self.database_ids:
             logging.info("Searching "+database_id.database+": "+database_id.id)
@@ -101,16 +101,16 @@ class Compound:
             #Stop searching once a valid inchikey is found
             if self.inchikey is not None:
                 break
-        
+
         #If the inchikey occurs in a list of mycotoxin inchikeys, add mycotoxin label
         if self.inchikey in mycotoxin_inchikeys:
             self.mycotoxin = True
-    
+
     def summary(self) -> list:
         """ Create summary of compound properties """
         summary=[self.mibig_accession,self.index,self.name,self.inchikey]
         return summary
-    
+
 def prefilter_mibig_entries(mibig_path,mycotoxin_formulas):
     """ Find entries that contain a compound with a molecular formula matching a mycotoxin, or for which the molecular formula is unknown."""
     #List all .json files in MIBiG database
@@ -140,12 +140,12 @@ def prefilter_mibig_entries(mibig_path,mycotoxin_formulas):
                 filtered_entries.add(entry)
                 missing_formula_count+=1
                 break
-                
+
     print("Nr. of entries kept after filtering: "+str(len(filtered_entries)))
     print("Nr. of entries kept due to missing molecular formula: "+str(missing_formula_count))
-    return(filtered_entries)  
+    return(filtered_entries)
 
-def get_mycotoxin_mibig_data(filtered_entries,mycotoxin_inchikeys): 
+def get_mycotoxin_mibig_data(filtered_entries,mycotoxin_inchikeys):
     """ Find compounds in the MIBiG database that are considered mycotoxins, according to an inchikey match.
         Return list with compound properties (MIBiG accession, index, name and inchikey)."""
     mycotoxin_mibig_data=[]
@@ -215,7 +215,9 @@ def main(comptox_path,mibig_path):
 if __name__ == "__main__":
     #Argument parsing
     parser = argparse.ArgumentParser()
-    parser.add_argument("comptox_path", type=str, help="Path to a .csv file containing a CompTox list of mycotoxins (url:https://comptox.epa.gov/dashboard/chemical-lists/MYCOTOX2)")
+    parser.add_argument("comptox_path", type=str, help=("Path to a .csv file containing a"
+                                                        " CompTox list of mycotoxins"
+                                                        " (url:https://comptox.epa.gov/dashboard/chemical-lists/MYCOTOX2)"))
     parser.add_argument("mibig_path", type=str, help="Path to a MIBiG database of annotations in .json format")
     parser.add_argument("-v","--verbose",help="Enable verbose mode",action="store_const",const=logging.INFO,dest="loglevel")
     args = parser.parse_args()
@@ -223,4 +225,3 @@ if __name__ == "__main__":
     logging.basicConfig(level=args.loglevel)
     #Run the main script
     main(args.comptox_path,args.mibig_path)
-    
